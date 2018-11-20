0db66299ee6230d0fb327f2c2b97068ff6565aa6
unknown <jigang.yf@hz.ali.com>
2018-11-20 10:18:11 +0800

    socketClient

--- src/main/java/com/ifan/SocketClient.java
+++ src/main/java/com/ifan/SocketClient.java
@@ -1,4 +1,15 @@
 package com.ifan;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class SocketClient {
+    public static void main(String[] args) throws IOException {
+        int port = 7000;
+
+        String host = "localhost";
+
+        Socket socket = new Socket(host,port);
+
+    }
 }
